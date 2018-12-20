3ffdc33d7da34f487a01f657079c015d7169e1df
Arateon <45388745+Arateon@users.noreply.github.com>
2018-12-20 11:02:04 +0100

    Delete dbh.inc.php

--- Website/includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "Vuurwerkshop";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
